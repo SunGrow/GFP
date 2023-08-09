64b725571193658600468bf09de622c0021dfd44
Lev Poliakov <SunGrow@tuta.io>
2023-08-09 07:59:29 +0200

    added a white dot

---
+++ Source/GFP/Private/Interfaces/ToggleEffect.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/ToggleEffect.h"
+
+
+// Add default functionality here for any IToggleEffect functions that are not pure virtual.
